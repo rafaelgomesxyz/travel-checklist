ef688937e23ea95dd42f74dbc064bd3bf73453fc
rafaelgssa <rafael.gssa@pm.me>
2019-11-20 19:31:58 -0300

    Add TripList

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:timeago/timeago.dart' as timeago;
+import 'package:travel_checklist/components/TripList.dart';
 import 'package:travel_checklist/screens/TripFormScreen.dart';
-import '../components/TripCard.dart';
-import '../models/Trip.dart';
 import 'TripListScreen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,44 +13,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Trip> _trips;
-
-  @override
-  void initState() {
-    super.initState();
-    timeago.setLocaleMessages('pt_BR', timeago.PtBrMessages());
-    this._resetState();
-  }
-
-  // Generate random trips until we have a database.
-  void _resetState() {
-    List<Trip> trips = [];
-    Random random = new Random();
-    int now = DateTime.now().millisecondsSinceEpoch;
-    for (int i = 0; i < 15; i++) {
-      int total = random.nextInt(9) + 1;
-      int current = random.nextInt(total) + 1;
-      Trip trip = Trip(i);
-      trip.destination = current > 5 ? 'Sao Paulo' : 'Rio de Janeiro';
-      trip.title = 'Viagem ${i + 1}';
-      trip.timestamp = now + current * 1000 * 60 * 60 * 60;
-      trip.progress.current = current;
-      trip.progress.total = total;
-      trips.add(trip);
-    }
-    setState(() {
-      this._trips = trips;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    print(this._trips.length);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh), onPressed: this._resetState ),
+          IconButton(icon: Icon(Icons.refresh), onPressed: () {} ),
         ],
       ),
       body: Container(
@@ -68,7 +34,10 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
             ),
-            ...(this._trips.length > 0 ? this._getList() : [Text('Nothing')]),
+            SizedBox(
+              child: TripList(numToShow: 3),
+              height: 200.0,
+            ),
             Container(
               child: ButtonTheme(
                 minWidth: 100.0,
@@ -140,44 +109,5 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
-  }
-
-  Widget buildList() {
-    return RefreshIndicator(
-      child: ListView(
-        children: this._getList(),
-        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 75.0),
-        //physics: AlwaysScrollableScrollPhysics(),
-      ),
-      onRefresh: () async {
-        this._resetState();
-        return;
-      },
-    );
-  }
-
-  List<Widget> _getList() {
-    this._trips.sort((Trip a, Trip b) {
-      if (a.progress.current == a.progress.total) {
-        return 1;
-      }
-      if (a.timestamp > b.timestamp) {
-        return 1;
-      }
-      if (a.timestamp < b.timestamp) {
-        return -1;
-      }
-      double aPercentage = a.progress.current / a.progress.total;
-      double bPercentage = b.progress.current / b.progress.total;
-      if (aPercentage > bPercentage) {
-        return 1;
-      }
-      if (aPercentage < bPercentage) {
-        return -1;
-      }
-      return 0;
-    });
-    this._trips.removeRange(3, this._trips.length);
-    return this._trips.map((Trip trip) => TripCard(trip)).toList();
   }
 }
