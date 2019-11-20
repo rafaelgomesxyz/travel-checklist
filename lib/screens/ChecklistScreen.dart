55357a3fe6167ca9106f06698877ff442f767d9e
heldosouza <heldo_augusto_@hotmail.com>
2019-11-20 19:40:24 -0300

    Add ChecklistScreen

---
+++ lib/screens/ChecklistScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class CheckListScreen extends StatefulWidget{
+  final String title;
+  CheckListScreen({ Key key, this.title }) : super(key: key);
+
+  @override
+  _ChecklistScreenState createState() => _ChecklistScreenState();
+}
+
+class _ChecklistScreenState extends State<CheckListScreen>{
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+
+}
