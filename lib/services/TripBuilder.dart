7a874cb2d0a115d2a82e88f1aea208a553c024d6
heldosouza <34147803+heldosouza@users.noreply.github.com>
2019-12-16 00:19:15 -0300

    Update TripBuilder.dart

--- lib/services/TripBuilder.dart
+++ lib/services/TripBuilder.dart
@@ -7,16 +7,34 @@ class TripBuilder {
   Map<String, List<String>> getPredefinedChecklists(Template template) {
     Map<String, List<String>> common = {
       'Acessórios': [
-        'Item1',
-        'Item2',
+        'Carteira',
+        'Bolsa',
+        'Câmera fotográfica',
+        'Carregador de celular',
+        'Carregador portátil',
+        'Documentos pessoais',
+        'Remédios',
       ],
       'Higiene Pessoal': [
-        'Item1',
-        'Item2',
+        'Condicionador',
+        'Cotonetes',
+        'Creme Dental',
+        'Desodorante',
+        'Escova de Cabelo',
+        'Escova de Dentes',
+        'Fio dental',
+        'Perfume',
+        'Protetor Solar',
+        'Shampoo',
       ],
       'Vestuário': [
-        'Item1',
-        'Item2',
+        'Blusas',
+        'Calças'
+        'Camisas',
+        'Chinelos',
+        'Meias',
+        'Sapatos',
+        'Roupa íntima',
       ],
     };
     switch (template) {
@@ -24,36 +42,60 @@ class TripBuilder {
         return {
           'Acessórios': [
             ...common['Acessórios'],
-            'Item3',
-            'Item4',
+            'Bóia',
+            'Bola de futebol',
+            'Chapéu',
+            'Guarda Sol',
+            'Óculos escuros',
           ],
           'Higiene Pessoal': [
             ...common['Higiene Pessoal'],
-            'Item3',
-            'Item4',
+            'Bronzeador',
+            'Protetor Solar',
+            'Toalha de banho',
           ],
           'Vestuário': [
             ...common['Vestuário'],
-            'Item3',
-            'Item4',
+            'Roupa de banho',
+            'Short',
+            'Camiseta regata',
+            'Protetor labial',
           ],
         };
       case Template.Campo:
         return {
           'Acessórios': [
             ...common['Acessórios'],
-            'Item3',
-            'Item4',
+            'Abridor de latas',
+            'Barraca',
+            'Canivete',
+            'Capa de chuva',    
+            'Coador de café',
+            'Cobertores',
+            'Colchão inflável',
+            'Corda',
+            'Garrafa de água',
+            'Lanterna',
+            'Mochila',
+            'Saco de dormir',
+            'Sacos de lixo',
+            'Talheres',
+            'Travesseiro',
           ],
           'Higiene Pessoal': [
             ...common['Higiene Pessoal'],
-            'Item3',
-            'Item4',
+            'Antisséptico',
+            'Repelente de insetos',
+            'Sabonete',
+            'Toalha de banho',
+            'Toalha de rosto',  
           ],
           'Vestuário': [
             ...common['Vestuário'],
-            'Item3',
-            'Item4',
+            'Bota impermeável',
+            'Casacos',
+            'Camisa de manga longa',
+            'Sapatos fechados',
           ],
         };
       case Template.Cidade:
